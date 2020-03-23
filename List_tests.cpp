8955d16da59941dc13a8f5dc7cb717b29193bd37
mgmii <mgmii@umich.edu>
2020-03-23 17:10:39 -0400

    Added unit tests for various funcs and debugged code

--- List_tests.cpp
+++ List_tests.cpp
@@ -5,11 +5,60 @@
 
 using namespace std;
 
-// Add your test cases here
+TEST(push_back) {
+    List<int> test;
+    test.push_back(5);
+    test.push_back(6);
+    ASSERT_EQUAL(*test.begin(),5);
+    ASSERT_EQUAL(*test.end(), 6);
+}
 
-TEST(test_stub) {
-    // Add test code here
-    ASSERT_TRUE(true);
+TEST(empty) {
+    List<int> test;
+    ASSERT_TRUE(test.empty());
+}
+
+TEST(back) {
+    List<int> test;
+    test.push_back(5);
+    ASSERT_EQUAL(test.back(), 5);
+}
+
+TEST(pop_back) {
+    List<int> test;
+    test.push_back(5);
+    test.push_back(6);
+    test.pop_back();
+    ASSERT_EQUAL(test.back(), 5);
+    test.pop_back();
+    ASSERT_TRUE(test.empty());
+}
+
+TEST(erase) {
+    List<int> test;
+    test.push_back(5);
+    test.push_back(6);
+    test.push_back(7);
+    test.push_back(8);
+    test.begin();
+    test.erase(++test.begin());
+    test.erase(test.begin());
+    test.erase(test.end());
+    test.erase(test.begin());
+    ASSERT_TRUE(test.empty());
+}
+
+TEST(not_equal_true) {
+    List<string> test1;
+    test1.push_back("HI");
+    test1.push_back("World");
+    ASSERT_TRUE(test1.begin() != test1.end());
+}
+
+TEST(not_equal_false) {
+    List<string> test;
+    test.push_back("Fish");
+    ASSERT_FALSE(test.begin() != test.end());
 }
 
 TEST_MAIN()
